3eef5d71dc687efb79bd1ac6aac0075d7363955c
arnabbhat82 <arnabbhat@gmail.com>
2019-09-25 10:32:34 +0530

    unsubscribe paramsSubscription

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.scss'],
 })
-export class ParentComponent implements OnInit {
+export class ParentComponent implements OnInit, OnDestroy {
   constructor(private route: ActivatedRoute) {}
   user: { id: number; name: string };
+  paramsSubscription: Subscription;
 
   ngOnInit() {
-    this.route.params.subscribe(({ id, name }) => {
+    this.paramsSubscription = this.route.params.subscribe(({ id, name }) => {
       this.user = { id, name };
     });
+  }
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
   }
 }
